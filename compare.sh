b49629e1b473bb7d3d1cabfd18a52d25238e3964
cmotc <cmotc@openmailbox.org>
2015-12-21 13:57:12 -0500

    updated compare.sh. Not quite there yet. I need to pass an array of filenames to cat.

--- compare.sh
+++ compare.sh
@@ -5,5 +5,19 @@ echo "            lines  words   chars"
 echo -n "kitchen " && cat "../android-kitchen/menu" "../android-kitchen/scripts/check_rom" "../android-kitchen/scripts/init_kitchen" "../android-kitchen/scripts/check_binaries" "../android-kitchen/tools/formats.txt" | wc
 echo -n "scullery" && cat "./usr/bin/scullery" "./usr/lib/scullery/extract_rom" "./usr/lib/scullery/functions.sh" "./usr/lib/scullery/formats.txt" | wc
 
-#shlint "../android-kitchen/menu" "../android-kitchen/scripts/check_rom" "../android-kitchen/scripts/init_kitchen"
-shlint "./usr/bin/scullery" "./usr/lib/scullery/extract_rom" "./usr/lib/scullery/functions.sh"
+KITCHLINT=$(shlint "../android-kitchen/menu" "../android-kitchen/scripts/check_rom" "../android-kitchen/scripts/init_kitchen" "../android-kitchen/scripts/check_binaries" \
+            3>&1 1>&2 2>&3 3>&-)
+echo "
+Linting currently cloned Android Kitchen scripts
+
+$KITCHLINT
+"
+[ "$KITCHLINT" = "" ] && echo "test1"
+SCULLINT=$(shlint "./usr/bin/scullery" "./usr/lib/scullery/extract_rom" "./usr/lib/scullery/functions.sh" \
+            3>&1 1>&2 2>&3 3>&-)
+echo "
+Linting current Android Scullery scripts
+
+$SCULLINT
+"
+[ "$SCULLINT" = "" ]&& echo "Static analysis complete, shlint found no errors or bashisms in Scullery"
