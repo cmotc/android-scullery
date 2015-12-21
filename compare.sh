4ff541981f64004fece2ccf780aa81fb6867916e
cmotc <cmotc@openmailbox.org>
2015-12-21 13:40:06 -0500

    updated hygiene script

--- compare.sh
+++ compare.sh
@@ -2,7 +2,8 @@
 #KITCHEN={ "../android-kitchen/menu" "../android-kitchen/scripts/check_rom" "../android-kitchen/scripts/init_kitchen" "../android-kitchen/tools/formats.txt" }
 #SCULLERY={ "./usr/bin/scullery" "./usr/lib/scullery/extract_rom" "./usr/lib/scullery/functions.sh" "./usr/lib/scullery/formats.txt" }
 echo "            lines  words   chars"
-echo -n "kitchen " && cat "../android-kitchen/menu" "../android-kitchen/scripts/check_rom" "../android-kitchen/scripts/init_kitchen" "../android-kitchen/tools/formats.txt" | wc && echo -n ""
-echo -n "scullery" && cat "./usr/bin/scullery" "./usr/lib/scullery/extract_rom" "./usr/lib/scullery/functions.sh" "./usr/lib/scullery/formats.txt" | wc && echo -n ""
+echo -n "kitchen " && cat "../android-kitchen/menu" "../android-kitchen/scripts/check_rom" "../android-kitchen/scripts/init_kitchen" "../android-kitchen/scripts/check_binaries" "../android-kitchen/tools/formats.txt" | wc
+echo -n "scullery" && cat "./usr/bin/scullery" "./usr/lib/scullery/extract_rom" "./usr/lib/scullery/functions.sh" "./usr/lib/scullery/formats.txt" | wc
 
-
+#shlint "../android-kitchen/menu" "../android-kitchen/scripts/check_rom" "../android-kitchen/scripts/init_kitchen"
+shlint "./usr/bin/scullery" "./usr/lib/scullery/extract_rom" "./usr/lib/scullery/functions.sh"
