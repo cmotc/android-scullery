f14a4c4b734a5aacf0fbbb854bc36d1f52fe993d
cmotc <cmotc@openmailbox.org>
2015-12-24 19:35:43 -0500

    made variable names more consistent

--- usr/lib/scullery/functions.sh
+++ usr/lib/scullery/functions.sh
@@ -3,7 +3,7 @@
 #runnable in the kitchen. Instead just include
 
 #This creates a list of whitespace separated pairs representing the files in the
-#configured ROM search path($romdir). The output can be passed to dialog to
+#configured ROM search path($SCULLERY_ROM_DIR). The output can be passed to dialog to
 #create a ROM menu.
 checkout_romdir(){
     c=0
@@ -15,5 +15,26 @@ checkout_romdir(){
     echo $tmp
 }
 
+#This checks for the presence of a ROM's working folder in the $SCULLERY_ROM_DIR and
+#checks if there is already a working directory associated with that ROM.
+#Re-naming ROM files is the only way of changing the working folder name, when
+#it needs to be done programmatically, make a backup, copy the backup to the
+#new name, then reload it.
+do_backup(){
+    FOLDER="$1"
+    FIND_WORKING=$(ls -d "$FOLDER.scullery")
+    UT=$(date +%s)
+    tar -cJf "$FIND_WORKING$UT.tar.xz" "$FIND_WORKING"  
+
+}
+
+#This takes a ROM from a file and places the files into the working directory.
+#It determines what kind of file it is itself.
 #
-#
+do_unpack(){
+    COMPRESSED_FILE="$1"
+    COMPRESSED_FILE_EXTENSION="${COMPRESSED_FILE##*.}"
+    if [ COMPRESSED_FILE_EXTENSION = ".tar.xz" ]; then
+        echo test
+    fi
+}
